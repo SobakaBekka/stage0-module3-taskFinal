37c56b63e1d420bd4eed98a5071238d99b12175a
SobakaBekka <st69704@upce.cz>
2024-02-18 23:55:50 +0100

    done the tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int thousends = number / 1000;
+        int hundreds = (number % 1000) / 100;
+        int tens = (number % 100) / 10;
+        int once = number % 10;
+        int summaDigits = thousends + hundreds + tens + once;
+        System.out.println(summaDigits);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
-    }
+  public void revert(int number) {
+
+      int hundreds = number / 100;//извлекаеи сотни
+      int tens = (number % 100) / 10;//извлекаеи десятки
+      int once =  number % 10;//извлекаеи единицы
+      int reverseNumber = once * 100 + tens * 10 + hundreds;//Возвращаем в обратном порядке
+      System.out.println(reverseNumber);
+  }
 }
